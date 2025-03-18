75c20c855077aab2224cc047dd17dacbe6caedac
daniel_victorr <danielvictor1408@gmail.com>
2025-03-18 10:38:01 -0300

    primeiro comiit

---
+++ souChurrasqueiro/src/components/Login.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import souChurras from "../assets/img/souChurras.jpg";
+import { TextField, Button, Box, Card, CardContent, Typography, Link } from "@mui/material";
+
+const Login = () => {
+  return (
+    <Box 
+      sx={{
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        height: "100vh", 
+        width: "100vw", // Garante que ocupa toda a largura da tela
+        bgcolor: "#bbdefb", // Azul mais claro para o fundo
+      }}
+    >
+      <Card sx={{ width: "90%", maxWidth: 400, p: 3, boxShadow: 3, bgcolor: "#ffffff" }}>
+        <CardContent sx={{ textAlign: "center" }}>
+          <Box display="flex" justifyContent="center" mb={2}>
+            <img src={souChurras} alt="Sou Churrasqueiro" style={{ width: "100px", borderRadius: "50%" }} />
+          </Box>
+          <Typography variant="h5" gutterBottom>
+            Bem vindo, faça seu login
+          </Typography>
+          <form>
+            <TextField
+              fullWidth
+              label="Digite seu email"
+              variant="outlined"
+              margin="normal"
+              required
+            />
+            <TextField
+              fullWidth
+              label="Digite sua senha"
+              variant="outlined"
+              type="password"
+              margin="normal"
+              required
+            />
+            <Button 
+              type="submit" 
+              variant="contained" 
+              color="primary" 
+              fullWidth 
+              sx={{ mt: 2 }}
+            >
+              Entrar
+            </Button>
+          </form>
+          <Typography variant="body2" sx={{ mt: 2 }}>
+            Ainda não tem uma conta? <Link href="/cadastro"> Cadastre-se</Link>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default Login;
